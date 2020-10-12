bb785f434147514d4efff795409758ef7fe06f13
Dhruv Sirohi <39976243+dhruv-sirohi@users.noreply.github.com>
2020-10-12 04:00:56 -0400

    Update Easy21.py

--- Easy21.py
+++ Easy21.py
@@ -3,25 +3,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-"""The game is played with an infinite deck of cards (i.e. cards are sampled
-with replacement)
-• Each draw from the deck results in a value between 1 and 10 (uniformly
-distributed). black = 2/3 prob
-• There are no aces or picture (face) cards in this game
-• At the start of the game both the player and the dealer draw one black
-card (fully observed)
-• Each turn the player may either stick or hit
-• If the player hits then she draws another card from the deck
-• If the player sticks she receives no further cards
-• The values of the player’s cards are added (black cards) or subtracted (red
-cards)
-• If the player’s sum exceeds 21, or becomes less than 1, then she “goes
-bust” and loses the game (reward -1)
-• If the player sticks then the dealer starts taking turns. The dealer always
-sticks on any sum of 17 or greater, and hits otherwise. If the dealer goes
-bust, then the player wins; otherwise, the outcome – win (reward +1),
-lose (reward -1), or draw (reward 0) – is the player with the largest
-"""
+#Some debug/gameplay statements have been commented out, but left in.
 
 class Dealer:
     
@@ -242,7 +224,7 @@ def start():
         j = j+1
         
         if(i % 10**pwr == 0):
-                print(i)
+                
                 q_val = []
                 X = []
                 Y = []
@@ -259,8 +241,7 @@ def start():
                         Y.append(y+1)
                         Z.append(q_max)
                         #y is equal to shown card
-                #print(X)
-                #print(Y)
+                
                 fig = plt.figure()
                 ax = fig.add_subplot(111, projection='3d')
                 ax.scatter(np.array(Y),np.array(X),np.array(Z))
